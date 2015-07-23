8c60c06ffca0bc06b9bfd9be055fd4801c4be6cf
Robin den Hertog <r.denhertog@ultimaker.com>
2015-07-23 09:53:30 +0200

    the head of the JarJar is slightly bigger
    Signed-off-by: Robin den Hertog <r.denhertog@ultimaker.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -339,7 +339,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 #define X_MIN_POS -14
 #define Y_MAX_POS 124
 #define Y_MIN_POS 0
-#define Z_MAX_POS 130
+#define Z_MAX_POS 125
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
