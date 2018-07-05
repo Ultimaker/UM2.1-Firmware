1c1f8f1f458f70e4add1ffb0aafa155d8a577bc1
ckielstra <c.kielstra@ultimaker.com>
2018-07-05 10:48:17 +0200

    Made several functions static to show the reduced scope.

--- Marlin/UltiLCD2_menu_material.cpp
+++ Marlin/UltiLCD2_menu_material.cpp
@@ -49,13 +49,13 @@ static void lcd_menu_material_temperature_settings();
 static void lcd_menu_material_retraction_settings();
 static void lcd_menu_material_retraction_settings_per_nozzle();
 static void lcd_menu_material_settings_store();
-bool lcd_material_check_temperature(unsigned long temperature);
-bool lcd_material_check_bed_temperature(unsigned long temperature);
-bool lcd_material_check_fan_speed(unsigned long fanspeed);
-bool lcd_material_check_material_flow(unsigned long flow);
-bool lcd_material_check_material_diameter(double diameter);
-bool lcd_material_check_retraction_length(float length);
-bool lcd_material_check_retraction_speed(unsigned long speed);
+static bool lcd_material_check_temperature(unsigned long temperature);
+static bool lcd_material_check_bed_temperature(unsigned long temperature);
+static bool lcd_material_check_fan_speed(unsigned long fanspeed);
+static bool lcd_material_check_material_flow(unsigned long flow);
+static bool lcd_material_check_material_diameter(double diameter);
+static bool lcd_material_check_retraction_length(float length);
+static bool lcd_material_check_retraction_speed(unsigned long speed);
 
 static void cancelMaterialInsert()
 {
@@ -1488,38 +1488,38 @@ void lcd_material_store_current_material()
     }
 }
 
-bool lcd_material_check_temperature(unsigned long temperature)
+static bool lcd_material_check_temperature(unsigned long temperature)
 {
     return temperature == 0 || temperature > HEATER_0_MAXTEMP;
 }
 
-bool lcd_material_check_bed_temperature(unsigned long temperature)
+static bool lcd_material_check_bed_temperature(unsigned long temperature)
 {
     return temperature > BED_MAXTEMP;
 }
 
-bool lcd_material_check_fan_speed(unsigned long fanspeed)
+static bool lcd_material_check_fan_speed(unsigned long fanspeed)
 {
     return fanspeed > 100;
 }
 
-bool lcd_material_check_material_flow(unsigned long flow)
+static bool lcd_material_check_material_flow(unsigned long flow)
 {
     return flow > 1000;
 }
 
-bool lcd_material_check_material_diameter(double diameter)
+static bool lcd_material_check_material_diameter(double diameter)
 {
     return diameter < 0.1 || diameter > 10.0;
 }
 
-bool lcd_material_check_retraction_length(float length)
+static bool lcd_material_check_retraction_length(float length)
 {
     //More than 20mm retraction is not a valid value
     return length > (20 * EEPROM_RETRACTION_LENGTH_SCALE);
 }
 
-bool lcd_material_check_retraction_speed(unsigned long speed)
+static bool lcd_material_check_retraction_speed(unsigned long speed)
 {
     //More than 45mm/s is not a valid value
     return speed ==0 || speed > (45 * EEPROM_RETRACTION_SPEED_SCALE);
